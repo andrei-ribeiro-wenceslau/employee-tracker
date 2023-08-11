bfe9e72c3d9ab9e6596808c30b6f90867d78c391
Andrei <andrei.ribeirow@gmail.com>
2023-08-11 21:31:56 +1000

    adding sql schema, creating database, creating department, role and employee tables. Adding connection to database using sql2 package in the app.js file.

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql2")
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "minhasenha",
+    database: "employee_tracker"
+  });
