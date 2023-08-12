b3e8084c1eadf9362e01b112ec205dc10f6e351b
Andrei <andrei.ribeirow@gmail.com>
2023-08-12 12:35:35 +1000

    renaming seed.sql file to seeds.sql. Declaring startApp function to initialize inquirer prompt with possible choices for the user and adding a swith loop to call related function based on the user choice

--- app.js
+++ app.js
@@ -6,4 +6,59 @@ const db = mysql.createConnection({
     user: "root",
     password: "minhasenha",
     database: "employee_tracker"
-  });
+})
+
+function startApp() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "choice",
+                choices: [
+                    "View All Employees?",
+                    "View All Employee's By Roles?",
+                    "View all Emplyees By Deparments",
+                    "Update Employee",
+                    "Add Employee?",
+                    "Add Role?",
+                    "Add Department?"
+                ]
+            }
+        ])
+        .then((answers) => {
+            switch (answers.choice) {
+                case "View All Departments":
+                  viewDepartments();
+                  break;
+                case "View All Roles":
+                  viewRoles();
+                  break;
+                case "View All Employees":
+                  viewEmployees();
+                  break;
+                case "Add Department":
+                  addDepartment();
+                  break;
+                case "Add Role":
+                  addRole();
+                  break;
+                case "Add Employee":
+                  addEmployee();
+                  break;
+                case "Update Employee Role":
+                  updateEmployeeRole();
+                  break;
+                default:
+                  console.log("Goodbye!");
+                  db.end();
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            db.end();
+        });
+}
+
+startApp();
+
