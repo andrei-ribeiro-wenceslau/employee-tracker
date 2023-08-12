b41e4d7e2c17511a434d6a7557fd87c898e6d951
Andrei <andrei.ribeirow@gmail.com>
2023-08-12 13:47:47 +1000

    added viewDepartment, viewRoles and viewEmployees functions for when the user clicks on one of these options on the prompt and the applications rerturns the desired tables

--- app.js
+++ app.js
@@ -8,6 +8,36 @@ const db = mysql.createConnection({
     database: "employee_tracker"
 })
 
+// Function to view all departments
+function viewDepartments() {
+    db.query("SELECT department.name AS department FROM department", (err, results) => {
+        if (err) throw err;
+
+        console.table(results);
+        startApp();
+    });
+}
+
+// Function to view all roles
+function viewRoles() {
+    db.query("SELECT role.title AS role, role.salary, department.name AS department_name FROM role INNER JOIN department ON role.department_id = department.id", (err, results) => {
+        if (err) throw err;
+
+        console.table(results);
+        startApp();
+    });
+}
+
+// Function to view all employees
+function viewEmployees() {
+    db.query("SELECT employee.first_name, employee.last_name , role.title AS job_title, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id", (err, results) => {
+        if (err) throw err;
+
+        console.table(results);
+        startApp();
+    });
+}
+
 function startApp() {
     inquirer
         .prompt([
@@ -16,42 +46,44 @@ function startApp() {
                 message: "What would you like to do?",
                 name: "choice",
                 choices: [
-                    "View All Employees?",
-                    "View All Employee's By Roles?",
-                    "View all Emplyees By Deparments",
+                    "View all Employees",
+                    "View all Roles",
+                    "View all Departments",
                     "Update Employee",
-                    "Add Employee?",
-                    "Add Role?",
-                    "Add Department?"
+                    "Add Employee",
+                    "Add Role",
+                    "Add Department",
+                    "Exit"
                 ]
             }
         ])
         .then((answers) => {
             switch (answers.choice) {
-                case "View All Departments":
-                  viewDepartments();
-                  break;
-                case "View All Roles":
-                  viewRoles();
-                  break;
-                case "View All Employees":
-                  viewEmployees();
-                  break;
+                case "View all Departments":
+                    viewDepartments();
+                    break;
+                case "View all Roles":
+                    viewRoles();
+                    break;
+                case "View all Employees":
+                    viewEmployees();
+                    break;
                 case "Add Department":
-                  addDepartment();
-                  break;
+                    // addDepartment();
+                    break;
                 case "Add Role":
-                  addRole();
-                  break;
+                    // addRole();
+                    break;
                 case "Add Employee":
-                  addEmployee();
-                  break;
+                    // addEmployee();
+                    break;
                 case "Update Employee Role":
-                  updateEmployeeRole();
-                  break;
-                default:
-                  console.log("Goodbye!");
-                  db.end();
+                    // updateEmployeeRole();
+                    break;
+                case "Exit":
+                    console.log("Goodbye!");
+                    db.end();
+                    break;
             }
         })
         .catch((error) => {
